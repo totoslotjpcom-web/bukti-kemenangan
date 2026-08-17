85a5d4f892a76ce5b2b8fbab078498c10a18e79d
totoslotjpcom-web <totoslotjpcom@gmail.com>
2026-08-17 19:47:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
-  siteName: "TOTOSLOTJP",
-  tagline: "Galeri Bukti Kemenangan Member Totoslotjp",
+  siteName: "BUKTI KEMENANGAN TOTOSLOTJP",
+  tagline: "JOIN DAN RASAKAN SENSASI MAXWIN BERSAMA TOTOSLOTJP",
   logoUrl: "https://ik.imagekit.io/jpgrup/TTS/logo-tsj.png?updatedAt=1783369859269",
   faviconUrl: "https://ik.imagekit.io/jpgrup/TTS/fav-tsj.png?updatedAt=1783369882506",
   backgroundUrl: "https://ik.imagekit.io/jpgrup/TTS/49a90a56-d309-4b17-a062-2c0723534140.png",
